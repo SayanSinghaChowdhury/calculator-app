9a41bafef1bfcdc7f692609af5daf0f662f9047e
Sayan Singha Chowdhury <sayansinghachowdhury.dev@gmail.com>
2025-12-27 13:59:50 +0530

    Basic calculator input tag add state & buttons add Functions

--- src/components/BasicCalculator.tsx
+++ src/components/BasicCalculator.tsx
@@ -1,29 +1,68 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "./shadcnui/button";
 import { CardContent } from "./shadcnui/card";
 import { Input } from "./shadcnui/input";
 
 const BasicCalculator = () => {
+	const [inputOne, setInputOne] = useState("");
+	const [inputTwo, setInputTwo] = useState("");
+	const [inputTotal, setInputTotal] = useState("");
+
+	const applyPlus = () => {};
+
+	const applyMinus = () => {};
+
+	const applyMultiply = () => {};
+
+	const applyDivide = () => {};
+
+	const applyReset = () => {};
+
 	return (
 		<>
 			<CardContent className="grid grid-cols-4 gap-6">
 				<Input
 					className="col-span-2"
 					placeholder="Number 1 "
+					onChange={({ target }) => setInputOne(target.value)}
 				/>
 				<Input
 					className="col-span-2"
 					placeholder="Number 2"
+					onChange={({ target }) => setInputTwo(target.value)}
 				/>
 
-				<Input className="col-span-4" />
+				<Input
+					className="col-span-4"
+					onChange={({ target }) => setInputTotal(target.value)}
+				/>
 
-				<Button className="cursor-pointer">Add + </Button>
-				<Button className="cursor-pointer">Subtract - </Button>
-				<Button className="cursor-pointer">multiply X </Button>
-				<Button className="cursor-pointer">Divide / </Button>
+				<Button
+					className="cursor-pointer"
+					onClick={applyPlus}>
+					Add +
+				</Button>
+				<Button
+					className="cursor-pointer"
+					onClick={applyMinus}>
+					Subtract -
+				</Button>
+				<Button
+					className="cursor-pointer"
+					onClick={applyMultiply}>
+					multiply X
+				</Button>
+				<Button
+					className="cursor-pointer"
+					onClick={applyDivide}>
+					Divide /
+				</Button>
 
 				<Button
 					variant={"destructive"}
+					onClick={applyReset}
 					disabled
 					className="col-span-4 cursor-pointer">
 					Reset
